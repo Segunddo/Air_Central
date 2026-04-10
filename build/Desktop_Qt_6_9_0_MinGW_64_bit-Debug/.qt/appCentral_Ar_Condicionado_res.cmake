69528b24fadf6122ff4b23544f014d7884f6ec31
Segunddo <antoniosegundo.887@gmail.com>
2026-04-09 23:54:16 -0300

    Despolui a branch

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appCentral_Ar_Condicionado_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appCentral_Ar_Condicionado")
-set(working_dir "C:/Codes/Central_Ar_Condicionado/Central_Ar_Condicionado")
-set(src_and_dest_list
-
-)
-set(timestamp_file "C:/Codes/Central_Ar_Condicionado/Central_Ar_Condicionado/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appCentral_Ar_Condicionado_res.txt")
